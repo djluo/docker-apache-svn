f07e7dc2a47e48fe00ba7b6459c4fe6b64636aeb
djluo <dj.luo@baoyugame.com>
2015-08-28 11:50:45 +0800

    only gzip/del apache logs

--- gzip.sh
+++ gzip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find /svnroot/logs/ -type f -name "*gz"       -mtime +30 -exec rm -f {} \;
-find /svnroot/logs/ -type f -name "*log*[^z]" -mtime +1  -exec gzip  {} \;
+find /svnroot/logs/ -type f -name "*_log*gz"   -mtime +30 -exec rm -f {} \;
+find /svnroot/logs/ -type f -name "*_log*[^z]" -mtime +1  -exec gzip  {} \;
